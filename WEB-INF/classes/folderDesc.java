8dd2f3c4d54f24812d9f8401b6576adfba0b6f08
Michelle Naylor <mnaylor@cs.ualberta.ca>
2013-11-19 21:02:45 -0700

    forgot to add folderDesc file

---
+++ WEB-INF/classes/folderDesc.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.util.*;
+import oracle.sql.*;
+import oracle.jdbc.*;
+import util.User;
+import util.Db;
+import util.Photo;
+import util.ImageUploader;
+
+import org.apache.commons.fileupload.DiskFileUpload;
+import org.apache.commons.fileupload.FileItem;
+
+/**
+ * Servlet to upload a folder of images
+ *
+*/
+
+public class folderDesc extends HttpServlet {
+    private Db database;
+    private HttpSession session;
+    private String owner;
+    public String response_message;
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+	System.out.println("in folderdesc");
+	String date = "";
+	String location = "";
+	String subject = "";
+	String description = "";
+	int security = 2; // default is private
+	ArrayList<FileItem> all_files = null;
+	try {
+	    session = request.getSession(true);
+	    owner = (String) session.getAttribute("username");
+	    all_files = 
+		(ArrayList<FileItem>) session.getAttribute("folderPhotos");
+	} catch (Exception e) {
+	    response_message = e.getMessage();	    
+	}
+
+	DiskFileUpload fu = new DiskFileUpload();
+	try {
+	    /*
+	     * Parse the HTTP request to get the image stream
+	     */
+	    List<FileItem> FileItems = fu.parseRequest(request);
+
+	    /*
+	     * Process the uploaded items, assuming 1 image file uploaded
+	     */
+	    for (FileItem item : FileItems) {
+		if (item.isFormField()) {
+		    String fieldname = item.getFieldName();
+
+		    if (fieldname.equals("date")) {
+			date = item.getString();
+		    }
+		    else if (fieldname.equals("location")) {
+			location = item.getString();
+		    }
+		    else if (fieldname.equals("subject")) {
+			subject = item.getString();
+		    }
+		    else if (fieldname.equals("description")) {
+			description = item.getString();
+		    }
+		    else if (fieldname.equals("security")) {
+			String sec = item.getString();
+			System.out.println("security = " + sec);
+			security = Integer.parseInt(sec);
+		    }
+		}	     
+	    }
+	} catch (Exception e) {
+	    response_message = e.getMessage();
+	}
+	System.out.println("at new photo");
+	Photo image = new Photo(0, owner, date, location, subject, 
+				description, security);
+	
+	for (FileItem item : all_files) {
+	    ImageUploader iu = new ImageUploader(image, item);
+	    response_message = iu.upload_image();
+	}
+
+	System.out.println("after imageuploader");
+	//Output response to the client
+	response.sendRedirect("index.jsp");
+    }
+    
+}
